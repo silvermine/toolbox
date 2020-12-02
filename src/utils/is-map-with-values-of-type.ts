83e295a34091a39929245de4b63b0c92c647eca1
Ethan Smith <ethan@onebytegone.com>
2020-12-02 15:30:00 -0500

    feat: add isMapWithValuesOfType type guard

---
+++ src/utils/is-map-with-values-of-type.ts
@@ -0,0 +1,21 @@
+import { isStringUnknownMap } from '../types/StringUnknownMap';
+
+/**
+ * Type guard for `Record<string, TypeThatYouSpecify>`.
+ *
+ * @returns `true` if `o` is a map with `string` keys and values that pass the provided
+ * type guard.
+ */
+export function isMapWithValuesOfType<T>(guard: (x: any) => x is T, o: any): o is Record<string, T> {
+   if (!isStringUnknownMap(o)) {
+      return false;
+   }
+
+   for (let k of Object.keys(o)) {
+      if (!guard((o as any)[k])) {
+         return false;
+      }
+   }
+
+   return true;
+}
