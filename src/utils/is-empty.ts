eec9b3fcb77e520620f6b5b3f7d79c875d7e6f87
Craig Weber <crgwbr@gmail.com>
2021-06-22 09:05:53 -0400

    feat: Make isEmpty work for Sets (#31)

--- src/utils/is-empty.ts
+++ src/utils/is-empty.ts
@@ -2,6 +2,7 @@ import { isArray } from './is-array';
 import { isString } from './is-string';
 import { isArguments } from './is-arguments';
 import { isUndefined } from './is-undefined';
+import { isSet } from './is-set';
 
 /**
  * Checks if `o` is an empty object. An object is "empty" if it:
@@ -18,6 +19,9 @@ export function isEmpty(o: any): boolean {
    }
    if (isArray(o) || isString(o) || isArguments(o)) {
       return o.length === 0;
+   }
+   if (isSet(o)) {
+      return o.size === 0;
    }
    return Object.keys(o).length === 0;
 }
