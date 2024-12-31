83eef93c893d9af091aa29383235924818e3964e
Jonah Brupbacher <jonahtylerb@gmail.com>
2024-12-31 10:28:32 -0500

    fix: issues for Typescript version 5.7.2

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,4 +1,6 @@
-export function delay<T = undefined>(ms: number, value?: T): Promise<T> {
+export function delay(ms: number): Promise<undefined>;
+export function delay<T>(ms: number, value: T): Promise<T>;
+export function delay<T>(ms: number, value?: T): Promise<T | undefined> {
    return new Promise((resolve) => {
       setTimeout(() => { resolve(value); }, ms);
    });
