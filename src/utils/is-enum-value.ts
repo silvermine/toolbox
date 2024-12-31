e6f9d3890b6e583d7ebb5f662822a7288e79e818
Jonah Brupbacher <jonahtylerb@gmail.com>
2024-12-31 10:28:32 -0500

    fix: issues for Typescript version 5.7.2

--- src/utils/is-enum-value.ts
+++ src/utils/is-enum-value.ts
@@ -1,7 +1,7 @@
 /**
  * Type guard to check to see if the given value is a valid value for the enum.
  */
-export function isEnumValue<T>(enumType: T, value: unknown): value is T[keyof T] {
+export function isEnumValue<T extends object>(enumType: T, value: unknown): value is T[keyof T] {
    return (Object.keys(enumType) as Array<keyof T>)
       .map((key) => {
          return enumType[key];
